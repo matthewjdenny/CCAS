bcc6bc03a4ebf4a8ddffbd006ce727c9c2b9c4a9
Matt Denny <matthewjdenny@gmail.com>
2016-01-13 10:38:50 -0500

    start basic documentation

---
+++ R/Package_Documentation.R
@@ -0,0 +1,15 @@
+#' CCAS: A Model for Communication Network Content and Structure
+#'
+#' @section CCAS functions:
+#' This package will eventually provide functions to estimate a statistical model
+#' of communication network data including email and blog posts.
+#'
+#' @docType package
+#' @name SpeedReader
+NULL
+#> NULL
+
+#' @useDynLib CCAS
+#' @importFrom Rcpp sourceCpp
+NULL
+#> NULL
---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage("CCAS: A Model for Communication Network Content and Structure
+                          Version 0.1.1 created on 2016-01-13.
+                          copyright (c) 2015, Matthew J. Denny, Penn State University
+                          Type help('CCAS') to get started.
+                          Development website: github.com/matthewjdenny/CCAS
+                          Contact: mdenny@psu.edu")
+}
