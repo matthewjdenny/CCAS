1b32e1a4306ad83f7b6f8f8cc45a0b234c668633
Matt Denny <matthewjdenny@gmail.com>
2016-01-13 21:56:02 -0500

    example test of log space multinomial sampler

---
+++ tests/testthat/test_log_space_multinomial_sampler.R
@@ -0,0 +1,16 @@
+test_that("Log space multinomial sampler works", {
+    skip_on_cran()
+
+    # create an example distribution
+    set.seed(12345)
+    # have to assign variables to the global environment so they will run in
+    # automatic tests
+    dist <- round(runif(100, min = -500, max = -400))
+    # get the result
+    result <- test_internal_functions(
+        Test_Log_Space_Multinomial_Sampler = TRUE,
+        distribution = dist,
+        seed = 12345)
+
+})
+
