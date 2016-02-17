7597b2e7c06b5ab2f0a0f149d69fa8b023cef56d
Matt Denny <matthewjdenny@gmail.com>
2016-02-16 19:49:37 -0500

    Changed seed

--- tests/testthat/test_log_space_multinomial_sampler.R
+++ tests/testthat/test_log_space_multinomial_sampler.R
@@ -6,7 +6,7 @@ test_that("Log space multinomial sampler works", {
     # have to assign variables to the global environment so they will run in
     # automatic tests
     dist <- round(runif(100, min = -500, max = -400))
-    seed <- 12345
+    seed <- 123456
     # get the result
     result <- test_internal_functions(
         Test_Log_Space_Multinomial_Sampler = TRUE,
