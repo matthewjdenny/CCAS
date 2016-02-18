0c16c07017c22907d4a185bec86bdd9429d159bc
Matt Denny <matthewjdenny@gmail.com>
2016-02-18 10:34:32 -0500

    basic documentation for main function, cleaned things up as well

--- R/ccas.R
+++ R/ccas.R
@@ -1,9 +1,40 @@
+#' @title Infer Communication Content and Structure
+#' @description Performs inference on the content conditional structure of a
+#' text valued communication network.
+#'
+#' @param specification A formulat object of the form '~ euclidean(d = 2)' where
+#' d is the number of dimensions in the latent space that the user would like to
+#' include. may also include optional terms 'sender("covariate_name")',
+#' 'receiver("covariate_name")' and 'nodemix("covariate_name", base = value)'
+#' which are defined analogously to the arguments in the latentnet package.
+#' @param document_authors A vector recording the index of the sender of each
+#' document.
+#' @param document_edge_matrix A documents x actors matrix recording whether
+#' actor j was a recipient of message i.
+#' @param document_term_matrix A documents x unique terms matrix recording the
+#' count of unique term j in document i.
+#' @param covariate_data Defaults to NULL.
+#' @param vocabulary Defaults to NULL.
+#' @param interaction_patterns Defaults to 4.
+#' @param topics Defaults to 40.
+#' @param alpha Defaults to 1.
+#' @param beta Defaults to 0.01.
+#' @param iterations Defaults to 1000.
+#' @param metropolis_hastings_iterations Defaults to 500.
+#' @param target_accept_rate Defaults to 0.25.
+#' @param tollerance Defaults to 0.05.
+#' @return An object of class CCAS containing estimation results.
+#' @export
 ccas <- function(specification,
                  document_authors,
                  document_edge_matrix,
                  document_term_matrix,
                  covariate_data = NULL,
                  vocabulary = NULL,
+                 interaction_patterns = 4,
+                 topics = 40,
+                 alpha = 1,
+                 beta = 0.01,
                  iterations = 1000,
                  metropolis_hastings_iterations = 500,
                  target_accept_rate = 0.25,
@@ -16,19 +47,13 @@ ccas <- function(specification,
     }
 
     # possible terms for inclusion in model specification.
-    possible_terms <- c("euclidean","nodemix","sender","receiver","topic_model_parameters")
+    possible_terms <- c("euclidean","sender","receiver","nodemix")
 
     # parse the specification and return a list object.
     parsed_specifcation <- parsed_specifcation(specification,
                                                using_covariates,
                                                covariate_data,
                                                possible_terms)
-
-    # the following terms will be included in the specification string.
-    # interaction_patterns = 4,
-    # latent_space_dimensions = 2,
-    # alpha = 1,
-    # beta = 0.01
 
     # initialize an object of class CCAS to store everything. This will include
     # initializing all latent variables and organizing data in a format that is
