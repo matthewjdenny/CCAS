dc5d34f8a5eff165cead4b4bb90d3873d9a56147
Matt Denny <matthewjdenny@gmail.com>
2016-02-24 12:16:43 -0500

    more checks and object constructor

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -23,5 +23,49 @@ prepare_data <- function(document_authors,
         using_covariates <- TRUE
     }
 
+    # if no vocabulary was provided, get the column names from the
+    # document_term_matrix
+    if (is.null(vocabulary)) {
+        vocabulary <- colnames(document_term_matrix)
+    }
 
+    # make sure that the number of documents implied by all input data
+    # structures is equal.
+    if (nrow(document_edge_matrix) == nrow(document_term_matrix) &
+        nrow(document_term_matrix) == length(document_authors)) {
+        cat("Number of documents:",length(document_authors),"\n")
+    } else {
+        stop("nrow(document_edge_matrix), nrow(document_term_matrix), length(document_authors) must all be equal, you provided data objects implying a different number of documents.")
+    }
+
+    # calculate some corpus level statistics
+    vocabulary_size <- length(vocabulary)
+    cat("Vocabulary size:", vocabulary_size, "unique terms...\n")
+    num_actors <- ncol(document_edge_matrix)
+    cat("Number of actors:", num_actors, "\n")
+    num_documents <- nrow(document_edge_matrix)
+    num_edges <- sum(document_edge_matrix)
+    cat("Total number of message recipients:", num_edges, "\n")
+
+    ComNet_Object <- new("ComNet",
+        document_authors = document_authors,
+        document_term_matrix = document_term_matrix,
+        document_edge_matrix = document_edge_matrix,
+        vocabulary = vocabulary,
+        num_tokens = num_tokens,
+        num_actors = num_actors,
+        num_documents = num_documents,
+        num_edges = num_edges,
+        vocabulary_size = vocabulary_size,
+        token_word_type_list = token_word_type_list,
+        token_topic_assignment_list = token_topic_assignment_list,
+        network_covariates_list = network_covariates_list,
+        aggregate_network = aggregate_network)
+
+    # only assign if covariate_data is not NULL in order to aviod error
+    if (using_covariates) {
+        ComNet_Object@covariate_data <- covariate_data
+    }
+
+    return(ComNet_Object)
 }
