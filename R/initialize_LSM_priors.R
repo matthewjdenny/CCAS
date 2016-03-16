ae56f1d736bc1e61f4952f2f55cb34693c48ca8b
Matt Denny <matthewjdenny@gmail.com>
2016-03-16 15:51:14 -0400

    make sure all of these are single values and that we replicate where necessary

---
+++ R/initialize_LSM_priors.R
@@ -0,0 +1,20 @@
+initialize_LSM_priors <- function(CCAS_Object,
+                                  LSM_intercept_prior_variance,
+                                  LSM_intercept_prior_mean,
+                                  LSM_position_prior_variance,
+                                  LSM_position_prior_mean,
+                                  LSM_coefficient_prior_variance,
+                                  LSM_coefficient_prior_mean) {
+
+    # currently just set them, could be different for different clusters in the
+    # future
+    CCAS_Object@LSM_intercept_prior_variance = LSM_intercept_prior_variance
+    CCAS_Object@LSM_intercept_prior_mean = LSM_intercept_prior_mean
+    CCAS_Object@LSM_position_prior_variance = LSM_position_prior_variance
+    CCAS_Object@LSM_position_prior_mean = LSM_position_prior_mean
+    CCAS_Object@LSM_coefficient_prior_variance = LSM_coefficient_prior_variance
+    CCAS_Object@LSM_coefficient_prior_mean = LSM_coefficient_prior_mean
+
+    return(CCAS_Object)
+}
+
--- R/initialize_latent_variables.R
+++ R/initialize_latent_variables.R
@@ -4,10 +4,10 @@ initialize_latent_variables <- function(CCAS_Object){
     # first initialize instances of LSM variables centered at the prior
     # mean and variance
     using_coefficients <- CCAS_Object@ComNet_Object@using_covariates
-    intercepts <- rep(CCAS_Object@LSM_intercept_prior_mean[1],
+    intercepts <- rep(CCAS_Object@LSM_intercept_prior_mean,
                       CCAS_Object@interaction_patterns)
     if (using_coefficients) {
-        coefficients <- matrix(CCAS_Object@LSM_coefficient_prior_mean[1],
+        coefficients <- matrix(CCAS_Object@LSM_coefficient_prior_mean,
                                nrow = CCAS_Object@interaction_patterns,
                                ncol = CCAS_Object@number_of_covariates)
     } else {
@@ -15,19 +15,28 @@ initialize_latent_variables <- function(CCAS_Object){
                                nrow = CCAS_Object@interaction_patterns,
                                ncol = 2)
     }
-    latent_positions <- array(CCAS_Object@LSM_position_prior_mean[1],
+    latent_positions <- array(CCAS_Object@LSM_position_prior_mean,
                               dim = c(CCAS_Object@interaction_patterns,
                                       CCAS_Object@ComNet_Object@num_actors,
                                       CCAS_Object@latent_space_dimensions))
+
+    # need to make these into vectors since they are only a single value
+    # when they are used elsewhere
+    ipv <- rep(CCAS_Object@LSM_intercept_prior_variance,
+               CCAS_Object@interaction_patterns)
+    cpv <- rep(CCAS_Object@LSM_coefficient_prior_variance,
+               CCAS_Object@interaction_patterns)
+    ppv <- rep(CCAS_Object@LSM_position_prior_variance,
+               CCAS_Object@interaction_patterns)
 
     # sample the new parameter values for these values
     LSM_Params <- snipp(
         intercepts,
         coefficients,
         latent_positions,
-        intercept_proposal_variances = CCAS_Object@LSM_intercept_prior_variance,
-        coefficient_proposal_variances = CCAS_Object@LSM_coefficient_prior_variance,
-        latent_position_proposal_variances = CCAS_Object@LSM_position_prior_variance,
+        intercept_proposal_variances = ipv,
+        coefficient_proposal_variances = cpv,
+        latent_position_proposal_variances = ppv,
         using_coefficients)
 
     # now initialize token topic assignments
