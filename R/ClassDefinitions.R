7d033dcbf410ee98b5c90b3faa79a6741f65c3c5
Matt Denny <matthewjdenny@gmail.com>
2016-03-16 13:18:47 -0400

    move classes to shared file to prevent warning

---
+++ R/ClassDefinitions.R
@@ -0,0 +1,59 @@
+# an S4 class for ccas objects
+setClass(Class = "ComNet",
+         representation = representation(
+             document_authors = "numeric",
+             document_term_matrix = "matrix",
+             document_edge_matrix = "matrix",
+             covariate_data = "data.frame",
+             vocabulary = "character",
+             num_tokens = "numeric",
+             num_actors = "numeric",
+             num_documents = "numeric",
+             num_edges = "numeric",
+             vocabulary_size = "numeric",
+             token_word_type_list = "list",
+             token_topic_assignment_list = "list",
+             network_covariates_list = "list",
+             aggregate_network = "matrix",
+             blank_documents = "numeric",
+             using_covariates = "logical"
+         ),
+         validity = function(object) {
+             #              if (!"matrix" %in% class(object@network) & is.null(object@network)
+             #                  == FALSE) {
+             #                  stop("'network' must be a 'matrix' object or 'NULL'.")
+             #              }
+             return(TRUE)
+         }
+)
+
+# an S4 class for ccas objects
+setClass(Class = "CCAS",
+         representation = representation(
+             ComNet_Object = "ComNet",
+             MCMC_output = "list",
+             parameter_estimates = "data.frame",
+             topic_model_results = "list",
+             number_of_interaction_patterns = "numeric",
+             number_of_topics = "numeric",
+             latent_space_dimensions = "numeric",
+             formula = "formula",
+             alpha = "numeric",
+             beta = "numeric",
+             iterations = "numeric",
+             burnin = "numeric",
+             LSM_intercept_prior_variance = "numeric",
+             LSM_intercept_prior_mean = "numeric",
+             LSM_position_prior_variance = "numeric",
+             LSM_position_prior_mean = "numeric",
+             LSM_coefficient_prior_variance = "numeric",
+             LSM_coefficient_prior_mean = "numeric"
+         ),
+         validity = function(object) {
+             #              if (!"matrix" %in% class(object@network) & is.null(object@network)
+             #                  == FALSE) {
+             #                  stop("'network' must be a 'matrix' object or 'NULL'.")
+             #              }
+             return(TRUE)
+         }
+)
--- R/ccas.R
+++ R/ccas.R
@@ -6,8 +6,8 @@
 #' euclidean(d = 2)' where d is the number of dimensions in the latent space
 #' that the user would like to include, and Comnet is a ComNet object generated
 #' by the prepare_data() function. May also include optional terms
-#' 'sender("covariate_name")',
-#' 'receiver("covariate_name")' and 'nodemix("covariate_name", base = value)'
+#' 'sender("covariate_name")', receiver("covariate_name")',
+#' 'nodemix("covariate_name", base = value)' and netcov("network_covariate"),
 #' which are defined analogously to the arguments in the latentnet package.
 #' @param interaction_patterns Defaults to 4.
 #' @param topics Defaults to 40.
@@ -17,6 +17,12 @@
 #' @param metropolis_hastings_iterations Defaults to 500.
 #' @param target_accept_rate Defaults to 0.25.
 #' @param tollerance Defaults to 0.05.
+#' @param LSM_intercept_prior_variance Defaults to 5.
+#' @param LSM_intercept_prior_mean Defaults to 0.
+#' @param LSM_position_prior_variance Defaults to 5.
+#' @param LSM_position_prior_mean Defaults to 0.
+#' @param LSM_coefficient_prior_variance Defaults to 5.
+#' @param LSM_coefficient_prior_mean Defaults to 0.
 #' @return An object of class CCAS containing estimation results.
 #' @export
 ccas <- function(formula,
@@ -27,7 +33,13 @@ ccas <- function(formula,
                  iterations = 1000,
                  metropolis_hastings_iterations = 500,
                  target_accept_rate = 0.25,
-                 tollerance = 0.05) {
+                 tollerance = 0.05,
+                 LSM_intercept_prior_variance = 5,
+                 LSM_intercept_prior_mean = 0,
+                 LSM_position_prior_variance = 5,
+                 LSM_position_prior_mean = 0,
+                 LSM_coefficient_prior_variance = 5,
+                 LSM_coefficient_prior_mean = 0) {
 
     # possible terms for inclusion in model specification.
     possible_structural_terms <- c("euclidean")
