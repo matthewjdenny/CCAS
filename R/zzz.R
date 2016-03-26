a99936131479e566a7a557b563893e70fca124f8
Matt Denny <matthewjdenny@gmail.com>
2016-03-26 19:04:43 -0400

    version bump for lots of new features

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("CCAS: A Model for Communication Network Content and Structure
-Version 0.3.1 created on 2016-03-24.
+Version 0.3.2 created on 2016-03-26.
 copyright (c) 2016: Matthew J. Denny, Penn State University,
+                    Zachary M. Jones, Penn State University,
                     Bruce Desmarais, Penn State University,
                     Hanna Wallach, Microsoft Research
 Type help('CCAS') to get started.
