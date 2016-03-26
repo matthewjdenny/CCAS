d6e680b8c565466f98ca87df641f35a583196720
Matt Denny <matthewjdenny@gmail.com>
2016-03-26 14:02:26 -0400

    add in sleeps, plot for each IP

--- R/generate_output.R
+++ R/generate_output.R
@@ -5,16 +5,39 @@ generate_output <- function(CCAS_Object,
     # generate unnormalized topic model log likelihood trace plot and print out
     # Geweke statistic
     plot_topic_model_log_likelihood(CCAS_Object)
+    Sys.sleep(2)
 
     # generate LSM trace plots and print out Geweke diagnostics
     plot_interaction_pattern_log_likelihood(CCAS_Object)
+    Sys.sleep(2)
 
     # generate interaction pattern specific sub network sociomatrices
-    interaction_pattern_subnetworks <- generate_interaction_pattern_subnetworks(
+    ret_list <- generate_interaction_pattern_subnetworks(
         CCAS_Object)
 
     # assign output into a list
     CCAS_Object@model_output <- list(
-        interaction_pattern_subnetworks = interaction_pattern_subnetworks)
+        interaction_pattern_subnetworks = ret_list$interaction_pattern_networks,
+        emails_represented = ret_list$emails_represented)
+
+    # generate subnetwork plots for each interaction pattern
+    mean_posterior_positions <- vector(mode = "list",
+        length = CCAS_Object@interaction_patterns)
+    for (i in 1:CCAS_Object@interaction_patterns) {
+        res <- plot_interaction_pattern_network(
+            CCAS_Object,
+            i,
+            plot_color_category = NULL)
+        Sys.sleep(2)
+        mean_posterior_positions[[i]] <- res
+    }
+    # make sure it has interpretable names
+    names(mean_posterior_positions) <- paste("interaction_pattern_",
+        1:CCAS_Object@interaction_patterns, sep = "")
+
+    # assign the resulting posterior mean positions to out output list object
+    CCAS_Object@model_output$mean_posterior_positions <-
+        mean_posterior_positions
+
     return(CCAS_Object)
 }
