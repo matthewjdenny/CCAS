a7b20d5b0ad61d01e05cf10616005b96f3ec6684
Matt Denny <matthewjdenny@gmail.com>
2017-01-31 23:37:02 -0500

    generate forward and backward samples separately

--- tests/testthat/getting_it_right.R
+++ tests/testthat/getting_it_right.R
@@ -34,7 +34,7 @@ test_that("That we get it right", {
 
 
     # first lets try without covariates
-    result <- test_internal_functions(
+    forward_samples <- test_internal_functions(
         Getting_It_Right = TRUE,
         author_indexes = author_indexes,
         covariates = covars,
@@ -70,8 +70,48 @@ test_that("That we get it right", {
         num_ip = num_ip,
         num_ld = num_ld,
         total_number_of_tokens = total_word_count,
-        GiR_samples = GiR_samples)
+        GiR_samples = GiR_samples,
+        forward_sample = TRUE)
 
-    # no errors!
+    backward_samples <- test_internal_functions(
+        Getting_It_Right = TRUE,
+        author_indexes = author_indexes,
+        covariates = covars,
+        alpha_m = alpha_m,
+        beta_n = beta_n,
+        using_coefficients = TRUE,
+        intercept_prior_mean = 0,
+        intercept_prior_standard_deviation = 1,
+        intercept_proposal_standard_deviations = c(0.25,0.25),
+        coefficient_prior_mean = 0,
+        coefficient_prior_standard_deviation = 1,
+        coefficient_proposal_standard_deviations = c(0.25,0.25),
+        latent_position_prior_mean = 0,
+        latent_position_prior_standard_deviation =1,
+        latent_position_proposal_standard_deviations = c(0.25,0.25),
+        target_accept_rate = 0.25,
+        tollerance = 0.05,
+        update_size = 0.05,
+        seed = 12345,
+        iterations = 5,
+        metropolis_iterations = 50,
+        iterations_before_t_i_p_updates = 2,
+        update_t_i_p_every_x_iterations = 2,
+        perform_adaptive_metropolis = TRUE,
+        slice_sample_alpha_m = -2,
+        slice_sample_step_size = 1,
+        parallel = FALSE,
+        num_documents = num_documents,
+        words_per_doc = words_per_doc,
+        num_topics = num_topics,
+        num_terms = num_terms,
+        num_actors = num_actors,
+        num_ip = num_ip,
+        num_ld = num_ld,
+        total_number_of_tokens = total_word_count,
+        GiR_samples = GiR_samples,
+        forward_sample = FALSE)
+
+    # now we need to compare
 
 })
